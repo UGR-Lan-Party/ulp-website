592d61f832f7d0c6ac22a29968515e3568cb516b
Vipeba <victorperezbarrancodev@gmail.com>
2025-01-14 17:28:24 +0100

    Refactoring de imágenes

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -43,55 +43,68 @@ export const Competitions: CompetitionData[] = [
     id: "smash",
     name: "Super Smash Bros Ultimate",
   },
+  {
+    id: "brawl-stars",
+    name: "Brawl Stars",
+  },
 ];
 
 export const CompetitionsCards: CompetitionCardData[] = [
   {
-    id: "fc-25",
-    card: "../../public/assets/competitions/cards/fc25.png",
-    logo: "../../public/assets/competitions/game-logos/fc25.webp",
-    character: "../../public/assets/competitions/card-characters/character-fc25.png",
-  },
-  {
-    id: "minecraft",
-    card: "../../public/assets/competitions/backgrounds/minecraft.png",
-    logo: "../../public/assets/competitions/game-logos/minecraft.png",
-    character: "../../public/assets/competitions/card-characters/character-minecraft.png",
-  },
-  {
     id: "mario-kart",
-    card: "../../public/assets/competitions/backgrounds/mario-kart.png",
-    logo: "../../public/assets/competitions/game-logos/mario.png",
-    character: "../../public/assets/competitions/card-characters/character-mario.png",
-  },
-  {
-    id: "league-of-legends",
-    card: "../../public/assets/competitions/backgrounds/lol.jpeg",
-    logo: "../../public/assets/competitions/game-logos/lol.png",
-    character: "../../public/assets/competitions/card-characters/character-lol.webp",
-  },
-  {
-    id: "valorant",
-    card: "../../public/assets/competitions/backgrounds/valorant.png",
-    logo: "../../public/assets/competitions/game-logos/valorant.png",
-    character: "../../public/assets/competitions/card-characters/",
-  },
-  {
-    id: "rocket-league",
-    card: "../../public/assets/competitions/backgrounds/rocket-league.png",
-    logo: "../../public/assets/competitions/game-logos/rocket.png",
-    character: "../../public/assets/competitions/card-characters/character-rocket.png",
+    card: '../../public/assets/competitions/cards/mario-kart.png',
+    logo: '../../public/assets/competitions/logos/mario.png',
+    character: '../../public/assets/competitions/characters/character-mario.png',
   },
   {
     id: "wii-sports",
-    card: "../../public/assets/competitions/backgrounds/wii-sports.png",
-    logo: "../../public/assets/competitions/game-logos/wii.png",
-    character: "../../public/assets/competitions/card-characters/",
+    card: '../../public/assets/competitions/cards/wii.png',
+    logo: '../../public/assets/competitions/logos/wii.png',
+    character: '../../public/assets/competitions/characters/wii.png',
+  },
+  {
+    id: "valorant",
+    card: '../../public/assets/competitions/cards/valorant.png',
+    logo: '../../public/assets/competitions/logos/valorant.png',
+    character: '../../public/assets/competitions/characters/valorant.png',
+  },
+  {
+    id: "league-of-legends",
+    card: '../../public/assets/competitions/cards/lol.png',
+    logo: '../../public/assets/competitions/logos/lol.png',
+    character: '../../public/assets/competitions/characters/character-lol.webp',
+  },
+
+  {
+    id: "rocket-league",
+    card: '../../public/assets/competitions/cards/rocket.png',
+    logo: '../../public/assets/competitions/logos/rocket.png',
+    character: '../../public/assets/competitions/characters/character-rocket.png',
+  },
+  {
+    id: "brawl-stars",
+    card: '../../public/assets/competitions/cards/brawls-stars.png',
+    logo: '../../public/assets/competitions/logos/brawl-stars.png',
+    character:
+      '../../public/assets/competitions/characters/character-brawl-stars.webp',
   },
   {
     id: "smash",
-    card: "../../public/assets/competitions/backgrounds/smash.png",
-    logo: "../../public/assets/competitions-logos/smash.png",
-    character: "../../public/assets/competitions/card-characters/",
+    card: '../../public/assets/competitions/cards/smash.png',
+    logo: '../../public/assets/competitions/logos/smash.png',
+    character: '../../public/assets/competitions/characters/character-smash.png',
+  },
+  {
+    id: "fc-25",
+    card: '../../public/assets/competitions/cards/fc25.png',
+    logo: '../../public/assets/competitions/logos/fc25.webp',
+    character: '../../public/assets/competitions/characters/character-fc25.png',
+  },
+  {
+    id: "minecraft",
+    card: '../../public/assets/competitions/cards/minecraft.png',
+    logo: '../../public/assets/competitions/logos/minecraft.png',
+    character:
+      '../../public/assets/competitions/characters/character-minecraft.png',
   },
 ];
